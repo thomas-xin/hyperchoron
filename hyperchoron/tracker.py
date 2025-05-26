2671492ae3a5291db0d0f20357896ba997e65f12
Thomas <thomasxin@gmail.com>
2025-05-27 02:51:00 +1000

    semi-functional xm import

--- hyperchoron/tracker.py
+++ hyperchoron/tracker.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 import functools
 from math import ceil, hypot
+import os
 from types import SimpleNamespace
 from .mappings import (
 	org_instrument_selection, org_instrument_mapping,
@@ -8,7 +9,7 @@ from .mappings import (
 	percussion_mats,
 	c4, c1,
 )
-from .util import create_reader, transport_note_priority
+from .util import create_reader, transport_note_priority, temp_dir, sample_rate
 
 
 @dataclass(slots=True)
@@ -19,10 +20,10 @@ class OrgNote:
 	volume: int
 	panning: int
 
-def render_org(notes, instrument_activities, speed_info, ctx):
+def build_org(notes, instrument_activities, speed_info, ctx):
 	orig_ms_per_clock, real_ms_per_clock, scale, orig_step_ms, _orig_tempo = speed_info
 	speed_ratio = real_ms_per_clock / scale / orig_ms_per_clock
-	wait = round(orig_step_ms / speed_ratio)
+	wait = max(1, round(orig_step_ms / speed_ratio))
 	activities = list(map(list, instrument_activities.items()))
 	instruments = []
 	if sum(t[1][1] for t in activities) >= 12:
@@ -272,6 +273,14 @@ def load_org(file):
 # TODO: Finish implementation
 def load_xm(file):
 	print("Importing XM...")
+
+	import librosa
+	import numpy as np
+	import soundfile as sf
+	from .pcm import separate_audio
+	path = os.path.abspath(file)
+	tmpl = path.replace("\\", "/").rsplit("/", 1)[-1].rsplit(".", 1)[0]
+
 	if isinstance(file, str):
 		file = open(file, "rb")
 	with file:
@@ -279,22 +288,28 @@ def load_xm(file):
 		offs = 60
 		header_size = read(offs, 4)
 		song_length = read(offs + 4, 2)
+		loop_start = read(offs + 6, 2)
 		channel_count = read(offs + 8, 2)
 		pattern_count = read(offs + 10, 2)
 		instrument_count = read(offs + 12, 2)
+		flags = read(offs + 14, 2)
+		is_amiga = not flags & 1
+		# print(is_amiga)
 		tempo = read(offs + 16, 2)
-		ordering = read(offs + 20, 256, decode=False)
+		bpm = read(offs + 18, 2)
+		ordering = read(offs + 20, 256, decode=False)[:song_length]
 		offs += header_size
 
 		patterns = []
+		pattern_lengths = []
 		for i in range(pattern_count):
 			pattern_head_size = read(offs, 4)
 			pattern_rows = read(offs + 5, 2)
 			pattern_size = read(offs + 7, 2)
-			print(i, pattern_rows, pattern_size)
 			offs += pattern_head_size
 			pattern = read(offs, pattern_size, decode=False)
 			patterns.append(pattern)
+			pattern_lengths.append(pattern_rows)
 			offs += pattern_size
 
 		instruments = []
@@ -303,63 +318,236 @@ def load_xm(file):
 			name = read(offs + 4, 22, decode=False).rstrip(b"\x00").decode("utf-8").strip()
 			instrument = SimpleNamespace(
 				name=name,
-				sustain=False,
+				sample_count=0,
+				midi=[46] * 96,
+				pitches=[0] * 96,
 			)
 			instruments.append(instrument)
 			sample_count = read(offs + 27, 2)
 			if sample_count > 0:
 				sample_header_size = read(offs + 29, 4)
+			instrument.sample_count = sample_count
+			sample_mapping = read(offs + 33, 96, decode=False) if sample_count > 0 else [0] * 96
 			offs += instrument_head_size
+			sample_sustains = []
 			sample_sizes = []
+			sample_pitches = []
+			sample_bits = []
 			for j in range(sample_count):
 				sample_size = read(offs, 4)
-				sample_type = read(offs + 14, 1)
+				sample_loops = read(offs + 8, 4)
+				sample_vol = read(offs + 12, 1)
 				sample_sizes.append(sample_size)
-				name = read(offs + 18, 22, decode=False).rstrip(b"\x00").decode("utf-8").strip()
-				if sample_type & 3:
-					instrument.sustain = True
+				sample_fine = read(offs + 13, 1)
+				if sample_fine >= 128:
+					sample_fine -= 256
+				sample_type = read(offs + 14, 1)
+				sample_pitch = read(offs + 16, 1)
+				if sample_pitch >= 128:
+					sample_pitch -= 256
+				sample_relative = sample_pitch + sample_fine / 128
+				sample_pitches.append(sample_relative)
+				sample_reserved = read(offs + 17, 1)
+				sample_bit = 4 if sample_reserved == 0xad else 16 if sample_type & 16 else 8
+				sample_bits.append(sample_bit)
+				name = read(offs + 18, 22, decode=False).rstrip(b"\x00").decode("utf-8", "replace").strip()
+				if sample_type & 3 and sample_loops:
+					sample_sustains.append(True)
+				else:
+					sample_sustains.append(False)
 				offs += sample_header_size
-			offs += sum(sample_sizes)
+			for j in range(sample_count):
+				size = sample_sizes[j]
+				if not size:
+					continue
+				sample_delta = read(offs, size, decode=False)
+				bits = sample_bits[j]
+				if bits == 16:
+					sample = np.clip(np.cumsum(np.frombuffer(sample_delta, dtype=np.int16)) / 32767, -1, None)
+				elif bits == 8:
+					sample = np.clip(np.cumsum(np.frombuffer(sample_delta, dtype=np.int8)) / 127, -1, None)
+				else:
+					raise NotImplementedError(bits)
+				pitch = 2 ** (sample_pitches[j] / 12)
+				sr = round(pitch * 8363)
+				adjusted = librosa.resample(sample, orig_sr=sr, target_sr=sample_rate)
+
+				# XM carries NO reliable information about the instruments themselves; this means we have to analyse the samples to decide if they're percussion, and if they're not, analyse the actual pitch they play at!
+				D = librosa.stft(adjusted)
+				H, P = librosa.decompose.hpss(D)
+				rms_harm = librosa.feature.rms(S=H, hop_length=len(adjusted), center=False)
+				rms_perc = librosa.feature.rms(S=P, hop_length=len(adjusted), center=False)
+				harm = np.sum(rms_harm)
+				perc = np.sum(rms_perc)
+				if perc >= harm / 8:
+					if perc < harm / 3:
+						# Instrument may still be percussion; call stem separation to verify
+						sample_file = f"{temp_dir}{tmpl}-{i}-{j}.flac"
+						sf.write(sample_file, adjusted, sample_rate)
+						output_names = {k: tmpl + f"-{i}-{j}-" + v for k, v in dict(
+							Vocals="V",
+							Drums="D",
+							Bass="B",
+							Other="O",
+						).items()}
+						separate_audio("htdemucs_ft.yaml", sample_file, output_names)
+						drums, *_ = librosa.load(output_names["Drums"] + ".flac", sr=sample_rate, mono=True)
+						rms_orig = librosa.feature.rms(y=adjusted, frame_length=len(adjusted), hop_length=len(adjusted))
+						rms_drum = librosa.feature.rms(y=drums, frame_length=len(drums), hop_length=len(drums))
+						orig = np.sum(rms_orig)
+						drum = np.sum(rms_drum)
+					else:
+						drum, orig = perc, harm
+					if drum >= orig / 3:
+						# Instrument is more likely percussion; call further stem separation to identify
+						output_names = {k: tmpl + f"-{i}-{j}-" + v for k, v in dict(
+							Kick="K",
+							Snare="S",
+							Toms="T",
+							HH="H",
+							Ride="R",
+							Crash="C",
+						).items()}
+						separate_audio("MDX23C-DrumSep-aufr33-jarredou.ckpt", sample_file, output_names)
+						outputs = {k: librosa.load(v + ".flac", sr=sample_rate, mono=True)[0] for k, v in output_names.items()}
+						scales = [0.5, 1, 1, 1.5, 2, 2]
+						output_volumes = {k: scales[i] * np.sum(librosa.feature.rms(y=v, frame_length=len(v), hop_length=len(v))) for i, (k, v) in enumerate(outputs.items())}
+						highest = np.argmax(tuple(output_volumes.values()))
+						# target = tuple(output_volumes)[highest]
+						notes = [35, 38, 47, 42, 46, 49]
+						for k, p in enumerate(sample_mapping):
+							if p == j:
+								instrument.midi[k] = -1
+								instrument.pitches[k] = notes[highest]
+						offs += size
+						continue
+				f0, voiced_flag, _ = librosa.pyin(
+					adjusted,
+					sr=sample_rate,
+					fmin=librosa.note_to_hz("C2"),
+					fmax=librosa.note_to_hz("C7"),
+					resolution=1,
+					switch_prob=1,
+					fill_na=0,
+				)
+				average_note = round(np.mean(librosa.hz_to_midi(f0[f0 != 0])).item())
+				for k, p in enumerate(sample_mapping):
+					if p == j:
+						instrument.midi[k] = 73 if sample_sustains[j] else 46
+						instrument.pitches[k] = k + average_note - 60 + 11
+				offs += size
 	events = [
-		[0, 0, "header", 1, len(instruments) + 1, 8],
-		[1, 0, "tempo", tempo * 1000 * 8],
+		[0, 0, "header", 1, len(instruments) + 1, tempo],
+		[1, 0, "tempo", 2500000 / bpm * tempo],
 	]
 	@functools.lru_cache(maxsize=pattern_count)
 	def decode_pattern(p):
-		pattern = []
+		output = []
+		tick = 0
+		channel = 0
 		i = 0
+		last_params = {}
+
+		def note_off(channel, original_pitch=0):
+			if last_params.get(channel):
+				pitch = last_params[channel]["pitch"]
+				ins = last_params[channel]["ins"]
+				instrument = instruments[ins - 1]
+				iid = instrument.midi[pitch]
+				output.append([channel, tick, "note_off_c", 9 if iid == -1 else channel + 10, instrument.pitches[pitch], 0])
+				return pitch == original_pitch
+
+		breakafter = False
 		while i < len(p):
-			pitch = p[i]
-			assert pitch < 128, "Pattern compression not yet implemented!"
-			ins, vcol, eff, efp = p[i + 1:i + 5]
-			i += 5
-			pattern.append([0, len(pattern), "note_on_c", channel, pitch or e.pitch + c1, 127, 1, e.length])
+			priority = 2
+			pitch = ins = vcol = eff = efp = 0
+			dbyt = p[i]
+			if dbyt >= 128:
+				if dbyt & 1:
+					i += 1
+					pitch = p[i]
+				if dbyt & 2:
+					i += 1
+					ins = p[i]
+				if dbyt & 4:
+					i += 1
+					vcol = p[i]
+				if dbyt & 8:
+					i += 1
+					eff = p[i]
+				if dbyt & 16:
+					i += 1
+					efp = p[i]
+				i += 1
+			else:
+				pitch = dbyt
+				ins, vcol, eff, efp = p[i + 1:i + 5]
+				i += 5
+			if vcol < 16:
+				try:
+					vel = last_params[channel]["vel"]
+				except KeyError:
+					vel = 127
+			elif vcol <= 96:
+				vel = min(127, (vcol - 16) * 2)
+			else:
+				raise NotImplementedError(vel)
+			if eff == 0xd:
+				breakafter = True
+			if pitch == 97:
+				note_off(channel)
+			elif pitch == 0:
+				if ins == 0:
+					try:
+						ins = last_params[channel]["ins"]
+					except KeyError:
+						ins = 1
+				instrument = instruments[ins - 1]
+				iid = instrument.midi[pitch]
+				output.append([channel, tick, "control_c", 9 if iid == -1 else channel + 10, 7, vel])
+			else:
+				if pitch == 0:
+					try:
+						pitch = last_params[channel]["pitch"]
+					except KeyError:
+						pitch = 37
+				if ins == 0:
+					try:
+						ins = last_params[channel]["ins"]
+					except KeyError:
+						ins = 1
+				if note_off(channel, pitch):
+					priority = 1
+				instrument = instruments[ins - 1]
+				iid = instrument.midi[pitch]
+				events.append([channel, tick, "program_c", 9 if iid == -1 else channel + 10, iid])
+				output.append([channel, tick, "control_c", 9 if iid == -1 else channel + 10, 7, vel])
+				output.append([channel, tick, "note_on_c", 9 if iid == -1 else channel + 10, instrument.pitches[pitch], 127, priority])
+				last_params[channel] = {"pitch": pitch, "ins": ins, "vel": vel}
+			channel += 1
+			if channel >= channel_count:
+				channel = 0
+				tick += tempo
+				if breakafter:
+					break
+		for channel in range(channel_count):
+			note_off(channel)
+		return output, tick
+	tick = 0
 	for b in ordering:
-		for event in decode_pattern(patterns[b]):
-			pass
-	for i, ins in enumerate(instruments):
-		if i not in range(8, 16):
-			events.append([i + 2, 0, "program_c", i, midi_instrument_selection[org_instrument_mapping[ins.id]]])
-			channel = i
-			pitch = None
-		else:
-			channel = 9
-			if i == 8:
-				events.append([i + 2, 0, "program_c", channel, 0])
-			pitch = [35, 38, 42, 46, 47, 73, 35, 35][i - 8]
-		for j, e in enumerate(ins.events):
-			if e.panning != 255:
-				events.append([i + 2, e.tick, "control_c", channel, 10, (e.panning - 6) / 6 * 63 + 64])
-			if e.volume != 255:
-				events.append([i + 2, e.tick, "control_c", channel, 7, e.volume / 254 * (127 - min_vol) + min_vol])
-			if e.pitch != 255:
-				events.append([i + 2, e.tick, "note_on_c", channel, pitch or e.pitch + c1, 127, 1, e.length])
+		output, t = decode_pattern(patterns[b])
+		for event in output:
+			adjusted = event.copy()
+			adjusted[1] += tick
+			events.append(adjusted)
+		tick += t
+	return events
 
 
 def save_org(transport, output, instrument_activities, speed_info, ctx):
 	print("Exporting ORG...")
 	import struct
-	instruments, wait = list(render_org(transport, instrument_activities, speed_info, ctx=ctx))
+	instruments, wait = list(build_org(transport, instrument_activities, speed_info, ctx=ctx))
 	nc = 0
 	with open(output, "wb") as org:
 		org.write(b"\x4f\x72\x67\x2d\x30\x32")
